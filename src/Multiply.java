4ebd5bc1799938356c225947b7d5961dc2c6d874
jobkid <101791461+jobkid@users.noreply.github.com>
2022-03-22 14:09:28 +0900

    Add files via upload

---
+++ src/Multiply.java
@@ -0,0 +1,16 @@
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("구구단 3단 출력");
+		
+		for (int i=1;i<10;i++) {
+			
+			System.out.println("3*"+i+"="+(3*i));
+			
+		}
+		
+	}
+
+}
