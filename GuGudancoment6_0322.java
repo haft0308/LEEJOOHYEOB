4e21a237a3ed2dfc6f862e6411bb1b7285513f54
jobkid <101791461+jobkid@users.noreply.github.com>
2022-03-22 15:11:13 +0900

    Add files via upload

---
+++ GuGudancoment6_0322.java
@@ -0,0 +1,31 @@
+
+public class GuGudancoment6_0322 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=1; i<10; i++) {
+			for(int j=1; j<10; j++) {
+				System.out.print(i+"*"+j+"="+(i*j)+"\t");
+				
+				if(j==9){
+					System.out.println();	
+				}
+			}
+			
+			
+		}
+		System.out.println();
+		
+		for (int m=1; m<10; m++) {
+			for(int n=1; n<10; n++) {
+				System.out.print(n+"*"+m+"="+(n*m)+"\t");
+				if(n==9) {
+					System.out.println();
+				}
+			}
+		}
+		
+		
+	}
+
+}
